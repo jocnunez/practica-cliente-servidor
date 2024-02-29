0c440af4c6f4086dc49b1b0ef23e4c4c7eb698e9
jocnunez <jose.nunnez@gmail.com>
2024-02-29 20:16:59 +0100

    app a medias

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,26 @@
-console.log("hola mundo")
+const fastify = require('fastify')
+const mongoose = require('mongoose')
+
+const server = fastify({logger: true})
+const db = await mongoose.connect('mongodb://user:pwd@database:27017')
+
+
+
+
+// Ruta raiz
+server.get('/', (request, reply) => {
+    reply.send({ hello: 'world'})
+})
+
+// Todo: Ruta get (READ)
+server.get('/notas', (request, reply) => {
+    //con mongoose busco las notas
+    reply.send({ notas: '[las notas]'})
+})
+
+
+// Todo: Ruta post (CREATE)
+// Todo: Ruta put (UPDATE)
+// Todo: Ruta delete (DELETE)
+
+server.listen({port: 3000})
